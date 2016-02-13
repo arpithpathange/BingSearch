9c55c2f8684c7db92eb370db850c90d75ba5874c
arpithpathange <arpith.pathange@gmail.com>
2016-02-14 02:11:48 +0530

    raking ruby

---
+++ Rakefile
@@ -0,0 +1,7 @@
+
+require 'cucumber/rake/task'
+
+Cucumber::Rake::Task.new(:features) do |t|
+    cucumber_opts = "features   --format html --out report.html --format pretty "
+      
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,13 @@
 require 'watir-webdriver'
+require 'rake'
+
+require 'headless'
+headless = Headless.new
+headless.start
 
 browser = Watir::Browser.new:firefox
+
+
 
 Before do
   @browser = browser
